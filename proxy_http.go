456e64cc9880bdc38a8272200925723250358108
joesis <joesis@getlantern.org>
2018-06-06 01:50:58 -0700

    add more logs

--- proxy_http.go
+++ proxy_http.go
@@ -82,8 +82,9 @@ func (proxy *proxy) logInitialReadError(downstream net.Conn, err error) error {
 
 func (proxy *proxy) handle(ctx context.Context, downstreamIn io.Reader, downstream net.Conn, upstream net.Conn) error {
 	defer func() {
+		log.Debug("Closing downstream")
 		if closeErr := downstream.Close(); closeErr != nil {
-			log.Tracef("Error closing downstream connection: %s", closeErr)
+			log.Debugf("Error closing downstream connection: %s", closeErr)
 		}
 	}()
 
@@ -315,7 +316,7 @@ func (proxy *proxy) writeResponse(downstream io.Writer, req *http.Request, resp 
 	if err != nil && resp.Body != nil {
 		resp.Body.Close()
 	}
-	log.Debugf("Wrote response:\n%v", buffer.String())
+	log.Debugf("Wrote response, err: %v:\n%v", err, buffer.String())
 	return err
 }
 
